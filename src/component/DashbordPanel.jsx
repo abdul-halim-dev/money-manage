66d4047b16bfeef0e9081c53bc75238eca06c81f
abdul-halim-dev <jamesborn1518@gmail.com>
2024-11-20 14:22:51 +0600

    seven time commit

--- src/component/DashbordPanel.jsx
+++ src/component/DashbordPanel.jsx
@@ -1,143 +1,134 @@
 import React from 'react'
-import { FaChartSimple,FaChevronRight, FaKey, FaMoneyBill, FaMoneyCheck, FaRightFromBracket, FaScrewdriverWrench, FaUsers } from 'react-icons/fa6'
+import { FaChartSimple, FaChevronRight, FaKey, FaMoneyBill, FaMoneyCheck, FaRightFromBracket, FaScrewdriverWrench, FaUsers } from 'react-icons/fa6'
 import { Link } from 'react-router-dom'
 
 const DashbordPanel = () => {
-  return (
-    <div className='w-[95%] sm:w-[90%] bg-primary flex items-center justify-center   flex-col gap-4'>
+    return (
+        <div className='w-[95%] sm:w-[90%] bg-primary flex items-center justify-center   flex-col gap-4'>
+            <div className='w-full '>
+                <Link to={'/balanceTransfer'} className=' w-full bg-secondary h-full flex items-center rounded-tr-xl rounded-tl-xl   justify-between p-3'>
+                    <div className='flex items-center justify-center gap-4'>
+                        <div className='w-[40px] h-[40px] bg-active flex items-center justify-center rounded-full text-secondary'>
+                            <FaChevronRight className='text-lg' />
+                        </div>
+                        <span className=' text-[18px] font-semibold '> Balance Transfer </span>
+                    </div>
+                    <span className='text-lg font-bold '>
+                        <FaChevronRight />
+                    </span>
+                </Link>
 
- 
-        <div className='w-full '>
-        <Link to={'/balanceTransfer'} className=' w-full bg-secondary h-full flex items-center rounded-tr-xl rounded-tl-xl   justify-between p-3'>
-            <div className='flex items-center justify-center gap-4'>
-                <div className='w-[40px] h-[40px] bg-active flex items-center justify-center rounded-full text-secondary'>
-                    <FaChevronRight className='text-lg'/>
-                </div>
-                <span className=' text-[18px] font-semibold '> Balance Transfer </span>
+                <Link to={'/AccountInfo'} className=' w-full bg-secondary h-full flex items-center justify-between p-3'>
+                    <div className='flex items-center justify-center gap-4'>
+                        <div className='w-[40px] h-[40px] bg-active flex items-center justify-center rounded-full text-secondary'>
+                            <FaScrewdriverWrench className='text-lg' />
+                        </div>
+                        <span className=' text-[18px] font-semibold '> Account Info </span>
+                    </div>
+
+                    <span className='text-lg font-bold '>
+                        <FaChevronRight />
+                    </span>
+                </Link>
+
+                <Link to={'/changePassword'} className=' w-full bg-secondary rounded-bl-xl rounded-br-xl  h-full flex items-center justify-between p-3'>
+                    <div className='flex items-center justify-center gap-4'>
+                        <div className='w-[40px] h-[40px] bg-active flex items-center justify-center rounded-full text-secondary'>
+                            <FaKey className='text-lg' />
+                        </div>
+                        <span className=' text-[18px] font-semibold '> Change Password </span>
+                    </div>
+
+                    <span className='text-lg font-bold '>
+                        <FaChevronRight />
+                    </span>
+                </Link>
             </div>
 
-            <span className='text-lg font-bold '>
-                <FaChevronRight/>
-            </span>
-        </Link>
-        
-        <Link to={'/AccountInfo'} className=' w-full bg-secondary h-full flex items-center justify-between p-3'>
-            <div className='flex items-center justify-center gap-4'>
-                <div className='w-[40px] h-[40px] bg-active flex items-center justify-center rounded-full text-secondary'>
-                    <FaScrewdriverWrench className='text-lg'/>
-                </div>
-                <span className=' text-[18px] font-semibold '> Account Info </span>
+            <div className='w-full '>
+                <Link to={'/commission'} className=' w-full bg-secondary h-full flex items-center rounded-tr-xl rounded-tl-xl   justify-between p-3'>
+                    <div className='flex items-center justify-center gap-4'>
+                        <div className='w-[40px] h-[40px] bg-active flex items-center justify-center rounded-full text-secondary'>
+                            <FaChartSimple className='text-lg' />
+                        </div>
+                        <span className=' text-[18px] font-semibold '> Commissions </span>
+                    </div>
+
+                    <span className='text-lg font-bold '>
+                        <FaChevronRight />
+                    </span>
+                </Link>
+
+                <Link to={'/depositHistory'} className=' w-full bg-secondary h-full flex items-center justify-between p-3'>
+                    <div className='flex items-center justify-center gap-4'>
+                        <div className='w-[40px] h-[40px] bg-active flex items-center justify-center rounded-full text-secondary'>
+                            <FaMoneyCheck className='text-lg' />
+                        </div>
+                        <span className=' text-[18px] font-semibold '> Deposit History </span>
+                    </div>
+
+                    <span className='text-lg font-bold '>
+                        <FaChevronRight />
+                    </span>
+                </Link>
+
+                <Link to={'/withdrawHistory'} className=' w-full bg-secondary h-full flex items-center justify-between p-3'>
+                    <div className='flex items-center justify-center gap-4'>
+                        <div className='w-[40px] h-[40px] bg-active flex items-center justify-center rounded-full text-secondary'>
+                            <FaMoneyBill className='text-lg' />
+                        </div>
+                        <span className=' text-[18px] font-semibold '> Withdraw History </span>
+                    </div>
+
+                    <span className='text-lg font-bold '>
+                        <FaChevronRight />
+                    </span>
+                </Link>
+
+                <Link to={'/myTeam'} className=' w-full bg-secondary rounded-bl-xl rounded-br-xl  h-full flex items-center justify-between p-3'>
+                    <div className='flex items-center justify-center gap-4'>
+                        <div className='w-[40px] h-[40px] bg-active flex items-center justify-center rounded-full text-secondary'>
+                            <FaUsers className='text-lg' />
+                        </div>
+                        <span className=' text-[18px] font-semibold '> My Team </span>
+                    </div>
+
+                    <span className='text-lg font-bold '>
+                        <FaChevronRight />
+                    </span>
+                </Link>
+
+
             </div>
 
-            <span className='text-lg font-bold '>
-                <FaChevronRight/>
-            </span>
-        </Link>
-        
-        <Link to={'/changePassword'} className=' w-full bg-secondary rounded-bl-xl rounded-br-xl  h-full flex items-center justify-between p-3'>
-            <div className='flex items-center justify-center gap-4'>
-                <div className='w-[40px] h-[40px] bg-active flex items-center justify-center rounded-full text-secondary'>
-                    <FaKey className='text-lg'/>
-                </div>
-                <span className=' text-[18px] font-semibold '> Change Password </span>
-            </div>
+            <div className='w-full '>
+                <Link to={'/twoFactor'} className=' w-full bg-secondary h-full flex items-center rounded-tr-xl rounded-tl-xl   justify-between p-3'>
+                    <div className='flex items-center justify-center gap-4'>
+                        <div className='w-[40px] h-[40px] bg-active flex items-center justify-center rounded-full text-secondary'>
+                            <FaKey className='text-lg' />
+                        </div>
+                        <span className=' text-[18px] font-semibold '> Two Factor </span>
+                    </div>
 
-            <span className='text-lg font-bold '>
-                <FaChevronRight/>
-            </span>
-        </Link>
+                    <span className='text-lg font-bold '>
+                        <FaChevronRight />
+                    </span>
+                </Link>
+
+                <Link to={'/'} className=' w-full bg-secondary rounded-bl-xl rounded-br-xl  h-full flex items-center justify-between p-3'>
+                    <div className='flex items-center justify-center gap-4'>
+                        <div className='w-[40px] h-[40px] bg-active flex items-center justify-center rounded-full text-secondary'>
+                            <FaRightFromBracket className='text-lg' />
+                        </div>
+                        <span className=' text-[18px] font-semibold '> Log Out </span>
+                    </div>
+                    <span className='text-lg font-bold '>
+                        <FaChevronRight />
+                    </span>
+                </Link>
+            </div>
         </div>
- 
-        <div className='w-full '>
-        <Link to={'/commission'} className=' w-full bg-secondary h-full flex items-center rounded-tr-xl rounded-tl-xl   justify-between p-3'>
-            <div className='flex items-center justify-center gap-4'>
-                <div className='w-[40px] h-[40px] bg-active flex items-center justify-center rounded-full text-secondary'>
-                    <FaChartSimple className='text-lg'/>
-                </div>
-                <span className=' text-[18px] font-semibold '> Commissions </span>
-            </div>
-
-            <span className='text-lg font-bold '>
-                <FaChevronRight/>
-            </span>
-        </Link>
-        
-        <Link to={'/depositHistory'} className=' w-full bg-secondary h-full flex items-center justify-between p-3'>
-            <div className='flex items-center justify-center gap-4'>
-                <div className='w-[40px] h-[40px] bg-active flex items-center justify-center rounded-full text-secondary'>
-                    <FaMoneyCheck className='text-lg'/>
-                </div>
-                <span className=' text-[18px] font-semibold '> Deposit History </span>
-            </div>
-
-            <span className='text-lg font-bold '>
-                <FaChevronRight/>
-            </span>
-        </Link>
-        
-        <Link to={'/withdrawHistory'} className=' w-full bg-secondary h-full flex items-center justify-between p-3'>
-            <div className='flex items-center justify-center gap-4'>
-                <div className='w-[40px] h-[40px] bg-active flex items-center justify-center rounded-full text-secondary'>
-                    <FaMoneyBill className='text-lg'/>
-                </div>
-                <span className=' text-[18px] font-semibold '> Withdraw History </span>
-            </div>
-
-            <span className='text-lg font-bold '>
-                <FaChevronRight/>
-            </span>
-        </Link>
-
-        <Link to={'/myTeam'} className=' w-full bg-secondary rounded-bl-xl rounded-br-xl  h-full flex items-center justify-between p-3'>
-            <div className='flex items-center justify-center gap-4'>
-                <div className='w-[40px] h-[40px] bg-active flex items-center justify-center rounded-full text-secondary'>
-                    <FaUsers className='text-lg'/>
-                </div>
-                <span className=' text-[18px] font-semibold '> My Team </span>
-            </div>
-
-            <span className='text-lg font-bold '>
-                <FaChevronRight/>
-            </span>
-        </Link>
-        
-
-        </div>
- 
-        <div className='w-full '>
-        <Link to={'/twoFactor'} className=' w-full bg-secondary h-full flex items-center rounded-tr-xl rounded-tl-xl   justify-between p-3'>
-            <div className='flex items-center justify-center gap-4'>
-                <div className='w-[40px] h-[40px] bg-active flex items-center justify-center rounded-full text-secondary'>
-                    <FaKey className='text-lg'/>
-                </div>
-                <span className=' text-[18px] font-semibold '> Two Factor </span>
-            </div>
-
-            <span className='text-lg font-bold '>
-                <FaChevronRight/>
-            </span>
-        </Link>
-        
-       
-        
-        <Link to={'/'} className=' w-full bg-secondary rounded-bl-xl rounded-br-xl  h-full flex items-center justify-between p-3'>
-            <div className='flex items-center justify-center gap-4'>
-                <div className='w-[40px] h-[40px] bg-active flex items-center justify-center rounded-full text-secondary'>
-                    <FaRightFromBracket className='text-lg'/>
-                </div>
-                <span className=' text-[18px] font-semibold '> Log Out </span>
-            </div>
-
-            <span className='text-lg font-bold '>
-                <FaChevronRight/>
-            </span>
-        </Link>
-        </div>
- 
-       
-        
-    </div>
-  )
+    )
 }
 
 export default DashbordPanel
--- src/component/Usertask.jsx
+++ src/component/Usertask.jsx
@@ -1,65 +1,65 @@
-import React  from 'react'
- 
-import {  FaArrowDown, FaArrowUp, FaUser } from 'react-icons/fa6'
+import React from 'react'
+
+import { FaArrowDown, FaArrowUp, FaUser } from 'react-icons/fa6'
 import CountdownTimer from './CountdownTimer'
 import { Link } from 'react-router-dom'
 
 const Usertask = () => {
 
-     
- 
+
+
 
   return (
     <div className='w-[95%] sm:w-[90%]  flex items-center justify-center  flex-col gap-4 '>
 
-    <div className='w-full shadow-lg py-2 flex items-center justify-around  bg-secondary rounded-xl'>
+      <div className='w-full shadow-lg py-2 flex items-center justify-around  bg-secondary rounded-xl'>
 
-      <div className='flex items-center justify-center flex-col gap-1'>
-      <Link to={"/deposit"} className=' w-[50px] h-[50px] bg-active shadow-md flex items-center justify-center rounded-xl'>  
-            <FaArrowUp className='text-xl  text-secondary '/>
-        </Link>
-        <span>Deposit </span>
+        <div className='flex items-center justify-center flex-col gap-1'>
+          <Link to={"/deposit"} className=' w-[50px] h-[50px] bg-active shadow-md flex items-center justify-center rounded-xl'>
+            <FaArrowUp className='text-xl  text-secondary ' />
+          </Link>
+          <span>Deposit </span>
+        </div>
+        <div className='flex items-center justify-center flex-col gap-1'>
+          <Link to={"/myteam"} className=' w-[50px] h-[50px] bg-active shadow-md flex items-center justify-center rounded-xl'>
+            <FaUser className='text-xl  text-secondary ' />
+          </Link>
+          <span>Invite </span>
+        </div>
+        <div className='flex items-center justify-center flex-col gap-1'>
+          <Link to={"/withdraw"} className=' w-[50px] h-[50px] bg-active shadow-md flex items-center justify-center rounded-xl'>
+            <FaArrowDown className='text-xl  text-secondary ' />
+          </Link>
+          <span>Withdraw </span>
+        </div>
+
       </div>
-      <div className='flex items-center justify-center flex-col gap-1'>
-      <Link to={"/myteam"} className=' w-[50px] h-[50px] bg-active shadow-md flex items-center justify-center rounded-xl'>  
-            <FaUser className='text-xl  text-secondary '/>
-        </Link>
-        <span>Invite </span>
+
+      <div className='w-full flex items-center justify-between gap-4'>
+
+        <div className='w-[50%] bg-secondary shadow-lg rounded-xl p-3 flex items-start justify-center flex-col gap-1 '>
+          <span className='text-lg  font-medium '> Complete </span>
+          <h3 className=' text-xl font-bold '> 0 Task </h3>
+        </div>
+        <div className='w-[50%] bg-secondary shadow-lg rounded-xl p-3 flex items-start justify-center flex-col gap-1 '>
+          <span className='text-lg  font-medium '> Complete </span>
+          <h3 className=' text-xl font-bold '> 0 Task </h3>
+        </div>
+
       </div>
-      <div className='flex items-center justify-center flex-col gap-1'>
-      <Link to={"/withdraw"} className=' w-[50px] h-[50px] bg-active shadow-md flex items-center justify-center rounded-xl'>  
-            <FaArrowDown className='text-xl  text-secondary '/>
-        </Link>
-        <span>Withdraw </span>
-      </div>
 
-    </div>
-
-    <div className='w-full flex items-center justify-between gap-4'>
-
-    <div className='w-[50%] bg-secondary shadow-lg rounded-xl p-3 flex items-start justify-center flex-col gap-1 '>
-    <span className='text-lg  font-medium '> Complete </span>
-    <h3 className=' text-xl font-bold '> 0 Task </h3>
-    </div>
-    <div className='w-[50%] bg-secondary shadow-lg rounded-xl p-3 flex items-start justify-center flex-col gap-1 '>
-    <span className='text-lg  font-medium '> Complete </span>
-    <h3 className=' text-xl font-bold '> 0 Task </h3>
-    </div>
-
-    </div>
-
-    <div className='w-full flex items-start justify-start p-3 gap-3 bg-secondary  rounded-xl shadow-xl'>
+      <div className='w-full flex items-start justify-start p-3 gap-3 bg-secondary  rounded-xl shadow-xl'>
         <div className='w-[50px] h-[50px] '>
-            <img className='w-full h-full object-cover' src="https://i.ibb.co.com/tcqbVPf/pending.png" alt="" />
+          <img className='w-full h-full object-cover' src="https://i.ibb.co.com/tcqbVPf/pending.png" alt="" />
         </div>
         <div className='flex items-start justify-center flex-col gap-1'>
-            <span>Next Reminder </span>
-            <div className='text-2xl font-bold '>
-             <CountdownTimer duration={2 * 24 * 60 * 60 * 1000} />
-            
-            </div>
+          <span>Next Reminder </span>
+          <div className='text-2xl font-bold '>
+            <CountdownTimer duration={2 * 24 * 60 * 60 * 1000} />
+
+          </div>
         </div>
-     </div>
+      </div>
 
 
     </div>
