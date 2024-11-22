488889371803e1959b0acd40d632503ae7c22363
abdul-halim-dev <jamesborn1518@gmail.com>
2024-11-22 06:46:38 +0600

    sixteen commit

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,4 +1,4 @@
- 
+
 import { BsFillTelephoneOutboundFill } from 'react-icons/bs';
 import { FaAngleLeft, FaEnvelope, FaGlobe, FaLock, FaSackDollar, FaUser } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
@@ -11,121 +11,75 @@ const Signup = () => {
 
   return (
     <div className=' relative w-full   bg-primary flex items-center justify-center flex-col gap-3 py-[90px] p-4' >
-<div className=' absolute top-0  w-full flex items-center justify-between p-3'>
-  <Link className='text-active text-lg ' to={"/"}> <FaAngleLeft/> </Link>
-  <Link  className='text-active text-[16px] ' to={"/fulllogin"}>  
-Login
- </Link>
-</div>
+      <div className=' absolute top-0  w-full flex items-center justify-between p-3'>
+        <Link className='text-active text-lg ' to={"/"}> <FaAngleLeft /> </Link>
+        <Link className='text-active text-[16px] ' to={"/fulllogin"}>
+          Login
+        </Link>
+      </div>
 
 
       <div className=' w-full sm:w-[50%] shadow-md flex relative items-center justify-center gap-3 p-4 rounded-lg flex-col bg-secondary '>
 
 
         <form className=' pt-[70px] w-full flex items-start justify-center flex-col gap-1 '>
- 
+
           <div className='w-full flex items-center justify-center flex-col gap-6'>
- 
-         
-          <div className=' w-full flex items-center justify-center gap-0' >
-            <span className=' w-[13%] sm:w-[8%] bg-secondary h-[40px] rounded-tl-lg rounded-bl-lg border-2 flex items-center justify-center text-xl border-[#bbbbbb] '>   <FaUser />  </span>
-            <input className=' w-[87%]  sm:w-[92%] outline-none h-[40px] pl-3 rounded-tr-lg rounded-br-lg border-b-2 border-r-2 border-t-2 border-[#bbbbbb] '
-             type="text"
-             name="firstname"
-              id='firstname'
-              placeholder='First Name'
-              required
-            />
+
+            <div className=' w-full flex items-center justify-center gap-0' >
+              <span className=' w-[13%] sm:w-[8%] bg-secondary h-[40px] rounded-tl-lg rounded-bl-lg border-2 flex items-center justify-center text-xl border-[#bbbbbb] '>  <FaUser /> </span>
+              <input className=' w-[87%]  sm:w-[92%] outline-none h-[40px] pl-3 rounded-tr-lg rounded-br-lg border-b-2 border-r-2 border-t-2 border-[#bbbbbb] '
+                type="text"
+                name="username"
+                id='username'
+                placeholder='User Name'
+                required
+              />
+            </div>
+
+            {/* <div className=' w-full flex items-center justify-center gap-0' >
+              <span className=' w-[13%] sm:w-[8%] bg-secondary h-[40px] rounded-tl-lg rounded-bl-lg border-2 flex items-center justify-center text-xl border-[#bbbbbb] '>  <FaGlobe /> </span>
+              <select className=' w-[87%]  sm:w-[92%] outline-none h-[40px] pl-3 rounded-tr-lg rounded-br-lg border-b-2 border-r-2 border-t-2 border-[#bbbbbb] ' required name="country" id="country">
+                <option value="ban"> Bangladesh </option>
+                <option value="ind"> India </option>
+                <option value="pak"> Pakistan </option>
+                <option value="afgan"> Afganistan </option>
+                <option value="sri"> Srilanka </option>
+                <option value="nepal"> Nepal </option>
+                <option value="maldiv"> Maldiv </option>
+                <option value="canada"> Canada </option>
+              </select>
+            </div> */}
+
+
+            <div className=' w-full flex items-center justify-center gap-0' >
+              <span className=' w-[13%] sm:w-[8%] bg-secondary h-[40px] rounded-tl-lg rounded-bl-lg border-2 flex items-center justify-center text-xl border-[#bbbbbb] '>  <BsFillTelephoneOutboundFill /> </span>
+              <input className=' w-[87%]  sm:w-[92%] outline-none h-[40px] pl-3 rounded-tr-lg rounded-br-lg border-b-2 border-r-2 border-t-2 border-[#bbbbbb] ' type="tel" name="number"
+                id='number'
+                placeholder='Enter Number'
+                required
+              />
+            </div>
+
+
+
+            <div className=' w-full flex items-center justify-center gap-0' >
+              <span className='  w-[13%] sm:w-[8%] bg-secondary h-[40px] rounded-tl-lg rounded-bl-lg border-2 flex items-center justify-center text-xl border-[#bbbbbb] '>  <FaLock /> </span>
+              <input className='  w-[87%]  sm:w-[92%] outline-none h-[40px] pl-3 rounded-tr-lg rounded-br-lg border-b-2 border-r-2 border-t-2 border-[#bbbbbb] ' type="password" name="password"
+                id='password'
+                placeholder='Enter Password'
+                required
+              />
+            </div>
+
+
           </div>
 
-          
-          <div className=' w-full flex items-center justify-center gap-0' >
-            <span className=' w-[13%] sm:w-[8%] bg-secondary h-[40px] rounded-tl-lg rounded-bl-lg border-2 flex items-center justify-center text-xl border-[#bbbbbb] '>  <FaUser /> </span>
-            <input className=' w-[87%]  sm:w-[92%] outline-none h-[40px] pl-3 rounded-tr-lg rounded-br-lg border-b-2 border-r-2 border-t-2 border-[#bbbbbb] '
-             type="text"
-             name="lastname"
-              id='lastname'
-              placeholder='Last Name'
-              required
-            />
-          </div>
-
-          <div className=' w-full flex items-center justify-center gap-0' >
-            <span className=' w-[13%] sm:w-[8%] bg-secondary h-[40px] rounded-tl-lg rounded-bl-lg border-2 flex items-center justify-center text-xl border-[#bbbbbb] '>  <FaUser /> </span>
-            <input className=' w-[87%]  sm:w-[92%] outline-none h-[40px] pl-3 rounded-tr-lg rounded-br-lg border-b-2 border-r-2 border-t-2 border-[#bbbbbb] '
-             type="text"
-             name="username"
-              id='username'
-              placeholder='User Name'
-              required
-            />
-          </div>
-
-          
-          <div className=' w-full flex items-center justify-center gap-0' >
-            <span className=' w-[13%] sm:w-[8%] bg-secondary h-[40px] rounded-tl-lg rounded-bl-lg border-2 flex items-center justify-center text-xl border-[#bbbbbb] '>  <FaEnvelope /> </span>
-            <input className=' w-[87%]  sm:w-[92%] outline-none h-[40px] pl-3 rounded-tr-lg rounded-br-lg border-b-2 border-r-2 border-t-2 border-[#bbbbbb] '
-             type="email"
-             name="email"
-              id='email'
-              placeholder='Enter Email'
-              required
-            />
-          </div>
-
-          
-          <div className=' w-full flex items-center justify-center gap-0' >
-            <span className=' w-[13%] sm:w-[8%] bg-secondary h-[40px] rounded-tl-lg rounded-bl-lg border-2 flex items-center justify-center text-xl border-[#bbbbbb] '>  <FaGlobe /> </span>
-            <select  className=' w-[87%]  sm:w-[92%] outline-none h-[40px] pl-3 rounded-tr-lg rounded-br-lg border-b-2 border-r-2 border-t-2 border-[#bbbbbb] ' required name="country" id="country">
-            <option value="ban"> Bangladesh </option>
-            <option value="ind"> India </option>
-            <option value="pak"> Pakistan </option>
-            <option value="afgan"> Afganistan </option>
-            <option value="sri"> Srilanka </option>
-            <option value="nepal"> Nepal </option>
-            <option value="maldiv"> Maldiv </option>
-            <option value="canada"> Canada </option>
-            </select>
-             
-          </div> 
-
-
-          <div className=' w-full flex items-center justify-center gap-0' >
-            <span className=' w-[13%] sm:w-[8%] bg-secondary h-[40px] rounded-tl-lg rounded-bl-lg border-2 flex items-center justify-center text-xl border-[#bbbbbb] '>  <BsFillTelephoneOutboundFill /> </span>
-            <input className=' w-[87%]  sm:w-[92%] outline-none h-[40px] pl-3 rounded-tr-lg rounded-br-lg border-b-2 border-r-2 border-t-2 border-[#bbbbbb] ' type="tel" name="number"
-              id='number'
-              placeholder='Enter Number'
-              required
-            />
-          </div>
-
-
-
-          <div className=' w-full flex items-center justify-center gap-0' >
-            <span className='  w-[13%] sm:w-[8%] bg-secondary h-[40px] rounded-tl-lg rounded-bl-lg border-2 flex items-center justify-center text-xl border-[#bbbbbb] '>  <FaLock /> </span>
-            <input className='  w-[87%]  sm:w-[92%] outline-none h-[40px] pl-3 rounded-tr-lg rounded-br-lg border-b-2 border-r-2 border-t-2 border-[#bbbbbb] ' type="password" name="password"
-             id='password'
-             placeholder='Enter Password'
-             required
-            />
-          </div>
- 
-          <div className=' w-full flex items-center justify-center gap-0' >
-            <span className='  w-[13%] sm:w-[8%] bg-secondary h-[40px] rounded-tl-lg rounded-bl-lg border-2 flex items-center justify-center text-xl border-[#bbbbbb] '>  <FaLock /> </span>
-            <input className='  w-[87%]  sm:w-[92%] outline-none h-[40px] pl-3 rounded-tr-lg rounded-br-lg border-b-2 border-r-2 border-t-2 border-[#bbbbbb] ' type="password" name="password"
-             id='password'
-             placeholder='Confirm Password'
-             required
-            />
-          </div>
- 
-          </div>
-         
 
           <div class="flex items-center py-4">
-    <input id="link-checkbox" type="checkbox" value="" class="w-4 h-4 bg-active rounded-full "/>
-    <label for="link-checkbox" class="ms-2 text-sm  ">I agree with </label>
-</div>
+            <input id="link-checkbox" type="checkbox" value="" class="w-4 h-4 bg-active rounded-full " />
+            <label for="link-checkbox" class="ms-2 text-sm  ">I agree with </label>
+          </div>
 
 
 
@@ -137,10 +91,10 @@ Login
 
         </div>
 
-      <div className=' w-[90px] h-[90px] absolute -top-[40px] bg-secondary ring-[#2d8734] flex items-center justify-center flex-col rounded-full ring-4 '>
-      <span className='text-active text-2xl'>  <FaSackDollar/>  </span>
-      <p className='text-[12px] font-semibold uppercase '> Earn Shop </p>
-      </div>
+        <div className=' w-[90px] h-[90px] absolute -top-[40px] bg-secondary ring-[#2d8734] flex items-center justify-center flex-col rounded-full ring-4 '>
+          <span className='text-active text-2xl'>  <FaSackDollar />  </span>
+          <p className='text-[12px] font-semibold uppercase '> Earn Shop </p>
+        </div>
 
       </div>
 
